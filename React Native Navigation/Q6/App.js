4582fdaa0990bb38ea806d58b25109325ba52f83
Hunain Pervaiz <hunainpervaiz487@gmail.com>
2023-12-23 19:08:48 +0500

    next question

---
+++ React Native Navigation/Q6/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Screen1 from './Screens/Screen1';
+import Screen2 from './Screens/Screen2';
+import Screen3 from './Screens/Screen3';
+import Screen4 from './Screens/Screen4';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Screen1" component={Screen1} />
+      <Tab.Screen name="Screen2" component={Screen2} />
+      <Tab.Screen name="Screen3" component={Screen3} />
+      <Tab.Screen name="Screen4" component={Screen4} />
+    </Tab.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
